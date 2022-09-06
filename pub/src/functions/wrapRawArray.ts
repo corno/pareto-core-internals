673dcec2505597e48312b20307564edf95f6f488
corno <corno.astn.a@schraverus.com>
2022-09-06 11:17:54 +0200

    .

--- pub/src/functions/wrapRawArray.ts
+++ pub/src/functions/wrapRawArray.ts
@@ -1,4 +1,5 @@
 import * as pt from "pareto-core-types"
+import { createCounter } from "./createCounter"
 
 export function wrapRawArray<T>(source: T[]): pt.Array<T> {
     if (!(source instanceof Array)) {
@@ -9,11 +10,6 @@ export function wrapRawArray<T>(source: T[]): pt.Array<T> {
             return wrapRawArray(source.map((entry) => {
                 return callback(entry)
             }))
-        },
-        forEach: <NT>(
-            callback: (entry: T) => NT
-        ) => {
-            source.forEach(callback)
         },
         reduce: <NT>(
             initialValue: NT,
@@ -26,6 +22,47 @@ export function wrapRawArray<T>(source: T[]): pt.Array<T> {
 
             })
             return current
-        }
+        },
+        filter: <NT>(
+            cb: (v: T) => NT | undefined
+        ) => {
+            const filtered: NT[] = []
+            source.forEach(($) => {
+                const result = cb($)
+                if (result !== undefined) {
+                    filtered.push(result)
+                }
+            })
+            return wrapRawArray(filtered)
+        },
+        asyncMap: ($c) => {
+
+
+            function array<T, NT>(
+                array: T[],
+                elementCallback: ($: T) => pt.AsyncValue<NT>
+            ): pt.AsyncValue<pt.Array<NT>> {
+                return {
+                    execute: (cb) => {
+                        const temp: NT[] = []
+                        createCounter(
+                            (counter) => {
+                                array.forEach((v) => {
+                                    counter.increment()
+                                    elementCallback(v).execute((v) => {
+                                        temp.push(v)
+                                        counter.decrement()
+                                    })
+                                })
+                            },
+                            () => {
+                                cb(wrapRawArray(temp))
+                            }
+                        )
+                    }
+                }
+            }
+            return array(source, $c)
+        },
     }
 }
--- pub/src/functions/wrapRawDictionary.ts
+++ pub/src/functions/wrapRawDictionary.ts
@@ -1,13 +1,14 @@
-import { Dictionary } from "pareto-core-types"
+import * as pt from "pareto-core-types"
+import { createCounter } from "./createCounter"
 
 
-export function wrapRawDictionary<T>(source: { [key: string]: T }): Dictionary<T> {
+export function wrapRawDictionary<T>(source: { [key: string]: T }): pt.Dictionary<T> {
 
 
     type KeyValuePair<T> = { key: string, value: T }
 
     type DictionaryAsArray<T> = KeyValuePair<T>[]
-    
+
     function createDictionaryAsArray<X>(source: { [key: string]: X }): DictionaryAsArray<X> {
         const imp: DictionaryAsArray<X> = []
         Object.keys(source).forEach((key) => {
@@ -15,9 +16,9 @@ export function wrapRawDictionary<T>(source: { [key: string]: T }): Dictionary<T
         })
         return imp
     }
-    
-    function createDictionaryImp<X>(source: DictionaryAsArray<X>): Dictionary<X> {
-    
+
+    function createDictionaryImp<X>(source: DictionaryAsArray<X>): pt.Dictionary<X> {
+
         return {
             map: <NT>(callback: (entry: X, key: string) => NT) => {
                 return createDictionaryImp(source.map(($) => {
@@ -27,25 +28,32 @@ export function wrapRawDictionary<T>(source: { [key: string]: T }): Dictionary<T
                     }
                 }))
             },
-            forEach: (
-                sort,
-                callback
-            ) => {
-                const sortedKeys = source.map((entry, position) => {
-                    return {
-                        key: entry.key,
-                        position: position
-                    }
-                }).sort(
-                    (a, b) => {
-                        const after = sort(a.key, b.key)
-                        return after ? 1 : -1
-                    }
-                )
-                sortedKeys.forEach((sorted) => {
-                    callback(source[sorted.position].value, sorted.key)
-                })
-            },
+            // forEach: (
+            //     isFirstAfterSecond,
+            //     callback
+            // ) => {
+            //     const sortedKeys = source.map((entry, position) => {
+            //         return {
+            //             key: entry.key,
+            //             position: position
+            //         }
+            //     }).sort(
+            //         (a, b) => {
+            //             if(isFirstAfterSecond(a.key, b.key)) {
+            //                 return 1
+            //             } else {
+            //                 if (isFirstAfterSecond(b.key, a.key)) {
+            //                     return -1
+            //                 } else {
+            //                     return 0
+            //                 }
+            //             }
+            //         }
+            //     )
+            //     sortedKeys.forEach((sorted) => {
+            //         callback(source[sorted.position].value, sorted.key)
+            //     })
+            // },
             filter: <NT>(
                 cb: (v: X, key: string) => NT | undefined
             ) => {
@@ -66,16 +74,47 @@ export function wrapRawDictionary<T>(source: { [key: string]: T }): Dictionary<T
                 callback: (current: NT, entry: X, key: string) => NT,
             ) => {
                 let current = initialValue
-    
+
                 source.forEach(($) => {
                     current = callback(current, $.value, $.key)
-    
+
                 })
                 return current
+            },
+            asyncMap: ($c) => {
+                function imp<T, NT>(
+                    dictionary: DictionaryAsArray<T>,
+                    entryCallback: ($: T, key: string) => pt.AsyncValue<NT>
+                ): pt.AsyncValue<pt.Dictionary<NT>> {
+                    return {
+                        execute: (cb) => {
+                            const temp: { [key: string]: NT } = {}
+                            createCounter(
+                                (counter) => {
+                                    dictionary.map(($) => {
+                                        counter.increment()
+                                        entryCallback($.value, $.key).execute((nv) => {
+                                            temp[$.key] = nv
+                                            counter.decrement()
+                                        })
+                                    })
+                                },
+                                () => {
+                                    cb(wrapRawDictionary(temp))
+                                }
+                            )
+                        }
+                    }
+                }
+                return imp(
+                    source,
+                    $c,
+                )
+
             }
         }
     }
-    
+
     //first we clone the source data so that changes to that source will have no impact on this implementation.
     //only works if the set does not become extremely large
     const daa = createDictionaryAsArray(source)
