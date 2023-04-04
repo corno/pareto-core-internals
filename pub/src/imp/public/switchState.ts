74f4f350eaafe3e500310ca986e0fe2d6b4071fd
corno <corno.astn.a@schraverus.com>
2023-04-04 13:58:09 +0200

    .

--- pub/src/imp/public/switchState.ts
+++ pub/src/imp/public/switchState.ts
@@ -2,7 +2,7 @@
  * ss means 'switch state'.
  */
 
-export type State<T> = [string, T]
+export type State<T> = [string | boolean, T]
 
 export function ss<T, RT>($: State<T>, cb: ($: T) => RT): RT {
     return cb($[1])
