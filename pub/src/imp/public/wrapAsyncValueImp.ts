12f1f93969a04fcbdcc5cd2d799a4fbe72616777
corno <corno.astn.a@schraverus.com>
2023-04-11 09:31:57 +0200

    .

--- pub/src/imp/public/wrapAsyncValueImp.ts
+++ pub/src/imp/public/wrapAsyncValueImp.ts
@@ -3,30 +3,36 @@ import * as pt from "pareto-core-types"
 import { Execute } from "../types/Execute"
 
 
+export class AsyncValue<T> implements pt.AsyncValue<T> {
+    private execute: Execute<T>
+    constructor(execute: Execute<T>) {
+        this.execute = execute
+    }
+    map<NT>($v: ($: T) => AsyncValue<NT>): pt.AsyncValue<NT> {
+        function rewrite<In, Out>(
+            source: Execute<In>,
+            rewrite: (source: In) => pt.AsyncValue<Out>
+        ): pt.AsyncValue<Out> {
+            return wrapAsyncValueImp(
+                ((cb) => {
+                    source((v) => {
+                        rewrite(v).__execute(cb)
+                    })
+                })
+            )
+        }
+        return rewrite(this.execute, $v)
+    }
+
+    //////////
+    __execute ($i: ($: T) => void) {
+        this.execute($i)
+    }
+}
+
 export function wrapAsyncValueImp<T>(
     execute: Execute<T>,
 ): pt.AsyncValue<T> {
-    return {
-        map: ($v) => {
-            function rewrite<In, Out>(
-                source: Execute<In>,
-                rewrite: (source: In) => pt.AsyncValue<Out>
-            ): pt.AsyncValue<Out> {
-                return wrapAsyncValueImp(
-                    ((cb) => {
-                        source((v) => {
-                            rewrite(v).__execute(cb)
-                        })
-                    })
-                )
-            }
-            return rewrite(execute, $v)
-        },
-
-        //////////
-        __execute: ($i) => {
-            execute($i)
-        },
-    }
+    return new AsyncValue<T>(execute)
 
 }
