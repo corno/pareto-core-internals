ba51640d50cb4b30e43569780ffa483e28112047
corno <corno.astn.a@schraverus.com>
2023-04-13 14:06:19 +0200

    Revert "."
    This reverts commit b1cdc5be3366524b7f65199943a39973cdcc31c8.

--- pub/src/index.ts
+++ pub/src/index.ts
@@ -1,6 +1,7 @@
 
 export * from "./imp/public/wrapRawDictionary"
 export * from "./imp/public/wrapRawArray"
+export * from "./imp/public/flatten"
 export * from "./imp/public/panic"
 export * from "./imp/public/assertUnreachable"
 export * from "./imp/public/changeContext"
