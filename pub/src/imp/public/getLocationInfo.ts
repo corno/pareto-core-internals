63d56a4dc2b1f127199a97738660f62d13f2d9a4
Corno Schraverus <corno@schraverus.com>
2024-08-18 03:39:31 +0200

    comments and refactor

--- pub/src/imp/public/getLocationInfo.ts
+++ pub/src/imp/public/getLocationInfo.ts
@@ -7,33 +7,47 @@ export type SourceLocation = {
     'column': number,
 }
 
+/**
+ * 
+ * @returns the string on the specified line
+ */
+function getLine(e: Error, depth: number): string {
+    const regex = /\((.*)\)$/
+    //const regex = /\((.*):(\d+):(\d+)\)$/ //further splitted; file,line,column,
+    if (e.stack === undefined) {
+        throw new Error("NO STACK INFO")
+    }
+    const line = e.stack.split("\n")[depth + 2]
+    const match = regex.exec(line);
+
+    //determine the path relative to the current working directory
+    return path.relative(process.cwd(), (() => {
+        if (match === null) {
+            const begin = "    at /"
+            if (line.startsWith(begin)) {
+                return path.relative(process.cwd(), line.substring(begin.length - 1));
+            } else {
+                throw new Error(`COULD NOT PARSE STACK LINE: ${line}`)
+            }
+        } else {
+            return match[1]
+        }
+    })())
+
+}
+
+/**
+ * provides the source location (filepath and line number) of the source code file where this function is called,
+ * or if the depth is bigger than 0, the source location of the function at that stack depth
+ * @param depth 
+ * @returns 
+ */
 export function getLocationInfo(depth: number): SourceLocation {
 
+    //we create an error, not to be thrown but to be disected for its stack
     const e = new Error(); //don't move this statement to another function, it will change the depth of its stack
 
-
-    function getLine(): string {
-        const regex = /\((.*)\)$/
-        //const regex = /\((.*):(\d+):(\d+)\)$/ //further splitted; file,line,column,
-        if (e.stack === undefined) {
-            throw new Error("NO STACK INFO")
-        } const line = e.stack.split("\n")[depth + 2]
-        const match = regex.exec(line);
-        return path.relative(process.cwd(), (() => {
-            if (match === null) {
-                const begin = "    at /"
-                if (line.startsWith(begin)) {
-                    return path.relative(process.cwd(), line.substring(begin.length - 1));
-                } else {
-                    throw new Error(`COULD NOT PARSE STACK LINE: ${line}`)
-                }
-            } else {
-                return match[1]
-            }
-        })())
-
-    }
-    const line = getLine()
+    const line = getLine(e, depth)
     const split = line.split(":")
     if (split.length !== 3) {
         throw new Error(`UNEXPECTED LOCATION FORMAT (CHECK THE DEPTH PARAMETER): ${line} (Expected 'file:line:column')`)
