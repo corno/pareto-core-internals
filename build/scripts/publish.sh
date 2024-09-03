df9d06996f5ddfbfe13ecb890ba9d58d52001ee7
Corno Schraverus <corno@schraverus.com>
2024-09-03 11:18:38 +0200

    .

--- build/scripts/publish.sh
+++ build/scripts/publish.sh
@@ -9,7 +9,6 @@ if [ -z "$1" ]
     exit 1
 fi
 
-#the scriptsdir will be deleted, change to the root
 cd "$rootDir" && \
 
 #make sure everything is pushed
@@ -17,6 +16,8 @@ git push && \
 
 #validate that everything is committed and pushed (to make sure we're not messing with open work)
 git diff --exit-code && git log origin/master..master --exit-code && \
+
+npx update2latest --prefix $rootDir/pub && \
 
 $scriptDir/build.sh && \
 
